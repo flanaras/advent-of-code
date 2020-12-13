b637daed5a6eb85278ab3b811f69c1208e7f9896
Filippos Lanaras <github@flanaras.io>
2020-12-13 14:29:59 +0100

    add day 06 and day 07
    My solutions to day 06 and day 07, including input files and tests.

---
+++ src/test/kotlin/d07/MainTest.kt
@@ -0,0 +1,60 @@
+package d07
+
+import org.assertj.core.api.BDDAssertions.then
+import org.junit.jupiter.api.Test
+
+class MainTest {
+
+    @Test
+    fun should_count_part_a_properly() {
+        // given
+        val rules = Parser().parseFile()
+        val bagSecurityCount = BagSecurityCount(rules)
+
+        // when
+        val countOfBags = bagSecurityCount.count("shiny gold")
+
+        // then
+        then(countOfBags).isEqualTo(4)
+    }
+
+    @Test
+    fun should_count_part_b_properly() {
+        // given
+        val rules = Parser().parseFile()
+        val bagSecurity = BagSecurityCountB(rules)
+
+        // when
+        val countOfBags = bagSecurity.count("shiny gold")
+
+        // then
+        then(countOfBags).isEqualTo(32)
+    }
+
+    @Test
+    fun should_count_part_b_properly_second_input() {
+        // given
+        val rules = Parser().parseFile("day-07-test2")
+        val bagSecurity = BagSecurityCountB(rules)
+
+        // when
+        val countOfBags = bagSecurity.count("shiny gold")
+
+        // then
+        then(countOfBags).isEqualTo(126)
+    }
+
+    @Test
+    fun should_count_part_b_properly_question() {
+        // given
+        val rules = Parser().parseFile("day-07-a")
+        val bagSecurity = BagSecurityCountB(rules)
+
+        // when
+        val countOfBags = bagSecurity.count("shiny gold")
+
+        // then
+        then(countOfBags).isGreaterThan(2672)
+    }
+
+}
---
+++ src/test/kotlin/d07/ParserTest.kt
@@ -0,0 +1,61 @@
+package d07
+
+import org.assertj.core.api.BDDAssertions.then
+import org.junit.jupiter.api.Test
+
+class ParserTest {
+
+    private val parser = Parser()
+
+    @Test
+    fun should_parse_one_bag_rule() {
+        // given
+        val line = "dark red bags contain 2 dark orange bags."
+
+        // when
+        val bag = parser.parseLine(line)
+
+        // then
+        then(bag.owningBag).isEqualTo("dark red")
+        val darkBag = Bag("dark orange", 2)
+        then(bag.containsBags).hasSize(1)
+        then(bag.containsBags).contains(darkBag)
+    }
+
+    @Test
+    fun should_parse_two_bags_rule() {
+        // given
+        val line = "dark orange bags contain 3 bright white bags, 4 muted yellow bags."
+
+        // when
+        val bag = parser.parseLine(line)
+
+        // then
+        then(bag.owningBag).isEqualTo("dark orange")
+        val whiteBag = Bag("bright white", 3)
+        val yellowBag = Bag("muted yellow", 4)
+        then(bag.containsBags).containsAll(arrayListOf(whiteBag, yellowBag))
+    }
+
+    @Test
+    fun should_parse_four_bags_rule() {
+        // given
+        val line = "wavy teal bags contain 5 faded indigo bags, 4 dotted gray bags, 3 pale chartreuse bags, 3 vibrant coral bags."
+
+        // when
+        val bag = parser.parseLine(line)
+
+        // then
+        then(bag.owningBag).isEqualTo("wavy teal")
+        then(bag.containsBags).hasSize(4)
+
+        val bags = ArrayList<Bag>().apply {
+            add(Bag("faded indigo", 5))
+            add(Bag("dotted gray", 4))
+            add(Bag("pale chartreuse", 3))
+            add(Bag("vibrant coral", 3))
+        }
+
+        then(bag.containsBags).containsAll(bags)
+    }
+}
